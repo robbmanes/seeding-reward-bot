ae1ac308f7645ac17e2ea131a76476f7232b31d1
Robb Manes <robbmanes@protonmail.com>
2023-02-11 19:29:31 -0500

    Added logger for error tracking
    Added default GlowBot logger.

--- glowbot/bot.py
+++ glowbot/bot.py
@@ -1,4 +1,5 @@
 import discord
+import logging
 import tomli
 
 CONFIG_FILE = 'config.toml'
@@ -8,17 +9,25 @@ class Bot(discord.Client):
     def __init__(self):
         intents = discord.Intents.all()
         super().__init__(intents=intents)
-        self.load_config()
-    
-    def load_config(self, config_file=None):
+
+        self.config = self.get_config()
+        match self.config['bot']['log_level']:
+            case 'INFO':
+                logging.basicConfig(level=logging.INFO)
+            case 'DEBUG':
+                logging.basicConfig(level=logging.DEBUG)
+        self.logger = logging.getLogger('GlowBot')
+
+    def get_config(self):
         try:
             with open(CONFIG_FILE, "rb") as f:
-                self.config = tomli.load(f)
+                config = tomli.load(f)
+            return config
         except Exception as e:
-            print("Failed to parse configuration file %s: %s" % (CONFIG_FILE, e.message))
+            self.logger.fatal("Failed to parse configuration file %s: %s" % (CONFIG_FILE, e))
 
     async def on_ready(self):
-        print('Logged in as', self.user)
+        self.logger.info("GlowBot initialized.")
     
     async def on_message(self, message):
         if message.author == self.user:
