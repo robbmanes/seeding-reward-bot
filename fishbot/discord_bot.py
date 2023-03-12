2ea075fdff627d1fa43c0977c961a5314444e4b4
Robb Manes <robbmanes@protonmail.com>
2023-03-12 16:55:29 -0400

    Refactor base bot logic

--- fishbot/discord_bot.py
+++ fishbot/discord_bot.py
@@ -1,11 +1,16 @@
-import fishbot.cogs
+import asyncio
+from .config import Configuration
 import discord
 from discord.ext import commands
+import fishbot.cogs
 import logging
 import os
 
 
 class DiscordBot(commands.Bot):
+    """
+    Primary class representing a single discord bot belonging to a single server.
+    """
 
     def __init__(self, command_prefix):
         intents = discord.Intents.all()
@@ -19,3 +24,43 @@ class DiscordBot(commands.Bot):
                 await self.load_extension('fishbot.cogs.%s' % (cog))
             except Exception as e:
                 self.logger.error("Failed to load cog '%s': %s" % (cog, e))
+
+
+def run_discord_bot():
+    """primary execution point for the discord bot."""
+    # Parse configuration
+    logging.info("Parsing configuration file...")
+    try:
+        config = Configuration()
+    except Exception as e:
+        logging.fatal("Failed to parse configuration: %s" % (e))
+    
+    # Set logging level
+    logger = logging.getLogger(__package__)
+    match config.settings['fishbot']['log_level']:
+        case 'INFO':
+            logger.setLevel(logging.INFO)
+        case 'DEBUG':
+            logger.setLevel(logging.DEBUG)
+
+    # Start Discord bot
+    bot = DiscordBot(command_prefix='!')
+    env_token = os.environ.get('DISCORD_TOKEN')
+
+    if env_token is not None:
+        config.settings['discord']['discord_token'] = env_token
+
+    logger.info("Loading discord cog extensions...")
+    try:
+        asyncio.run(bot.load_extensions(config.settings['discord']['discord_cogs']))
+    except Exception as e:
+        logger.fatal("Failed to load cogs: %s" % (e))
+
+    logger.info("Starting discord services...")
+    try:
+        bot.run(config.settings['discord']['discord_token'], reconnect=True)
+    except Exception as e:
+        logger.fatal("Failed to run discord bot: %s" % (e))
+
+if __name__ == '__main__':
+    run_discord_bot()
