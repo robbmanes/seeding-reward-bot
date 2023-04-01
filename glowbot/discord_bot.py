c88d4b3be44caef654adcc1ecb6bae5be2f287f8
Robb Manes <robbmanes@protonmail.com>
2023-04-01 15:00:08 +0000

    Remove cog compatibility and break out database/config
    DB and Config should be handled globally in their own files, and removed
    plugin system entirely.  Eventually I plan to break out the
    hell_let_loose cog entirely into RCON clients that can act a bit more
    streamlined than running solely in the discord event loop.
    
    Ideally we'd have different processes; a Discord bot in a process, an
    RCON client in a process, a UI in a process, etc.  They can share the
    database asynchronously for data consistency.

--- glowbot/discord_bot.py
+++ glowbot/discord_bot.py
@@ -1,101 +1,48 @@
 import asyncio
-from .config import Configuration
+from glowbot.config import global_config
+from glowbot.db import GlowDatabase
 import discord
 from discord.ext import commands
 import logging
 import os
 import sys
-from tortoise import Tortoise
 import traceback
 
-class DiscordBot(commands.Bot):
-    """
-    Primary class representing a single discord bot belonging to a single server.
-    """
-
-    models = ['aerich.models', 'glowbot.hell_let_loose']
-
-    def __init__(self, command_prefix):
-        intents = discord.Intents.all()
-        super().__init__(command_prefix, intents=intents)
-
-        self.logger = logging.getLogger(__package__)
-    
-    async def init_db(self):
-        db_config = {
-            'connections': {
-                'default': {
-                    'engine': "tortoise.backends.asyncpg",
-                    'credentials': {
-                        'host': self.config['database']['postgres']['db_url'],
-                        'port': self.config['database']['postgres']['db_port'],
-                        'user': self.config['database']['postgres']['db_user'],
-                        'password': self.config['database']['postgres']['db_password'],
-                        'database': self.config['database']['postgres']['db_name'],
-                    },
-                },
-            },
-            'apps': {
-                'glowbot': {
-                    'models': self.models,
-                    'default_connection': 'default',
-                },
-            },
-        }
-
-        self.logger.info("Loading ORM for models: %s" % (self.models))
-        await Tortoise.init(config=db_config)
-        await Tortoise.generate_schemas()
-    
-    def load_config(self, config):
-        self.config = config
-
 def run_discord_bot():
-    """primary execution point for the discord bot."""
-    # Parse configuration
-    logging.info("Parsing configuration file...")
-    try:
-        config = Configuration()
-    except Exception as e:
-        logging.fatal("Failed to parse configuration: %s" % (e))
-        traceback.print_exc()
-    
     # Set logging level
-    logger = logging.getLogger(__package__)
-    match config.settings['glowbot']['log_level']:
+    match global_config['glowbot']['log_level']:
         case 'INFO':
-            logger.setLevel(logging.INFO)
+            logging.basicConfig(level=logging.INFO)
         case 'DEBUG':
-            logger.setLevel(logging.DEBUG)
+            logging.basicConfig(level=logging.DEBUG)
+
+    logger = logging.getLogger(__package__)
     
     # Check environment variables to override settings file
     env_token = os.environ.get('DISCORD_TOKEN')
 
     if env_token is not None:
-        config.settings['discord']['discord_token'] = env_token
+        global_config['discord']['discord_token'] = env_token
 
     # Discord bot logic
-    bot = DiscordBot(command_prefix='!')
-
-    # We load the configuration as late as possible to allow for customization.
-    bot.load_config(config.settings)
+    bot = commands.Bot(command_prefix='!')
 
     # Load the bot extension
     bot.load_extension('glowbot.hell_let_loose')
 
     # Load in the event loop for the database initialization
     loop = asyncio.get_event_loop()
+    db = GlowDatabase(loop)
+    bot.db = db
     bot.loop = loop
-    loop.run_until_complete(bot.init_db())
 
     # Actually run the bot.
     logger.info("Starting discord services...")
     try:
-        bot.run(bot.config['discord']['discord_token'], reconnect=True)
+        bot.run(global_config['discord']['discord_token'], reconnect=True)
     except Exception as e:
         logger.fatal("Failed to run discord bot: %s" % (e))
         traceback.print_exc()
-
 
 if __name__ == '__main__':
     run_discord_bot()
