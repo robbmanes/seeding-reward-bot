7e1b53c74877577f2fd7784b53c3fa4a1870fedb
Kevin Brasier <wonderfulweb@gmail.com>
2026-01-02 17:24:23 -0800

    Add unique constraints on player_id and discord_id

--- src/seeding_reward_bot/commands/util.py
+++ src/seeding_reward_bot/commands/util.py
@@ -3,7 +3,7 @@ import logging
 import discord
 from discord import ApplicationCommandInvokeError
 from discord.ext import commands
-from tortoise.exceptions import DoesNotExist, MultipleObjectsReturned
+from tortoise.exceptions import DoesNotExist
 
 from seeding_reward_bot.config import global_config
 from seeding_reward_bot.db import HLL_Player
@@ -42,8 +42,6 @@ class BotCommands(commands.Cog):
     async def get_player_by_player_id(player_id: str) -> HLL_Player:
         try:
             return await HLL_Player.get(player_id=player_id)
-        except MultipleObjectsReturned:
-            raise EphemeralAdminError(f"Found multiple players for {player_id=}")
         except DoesNotExist:
             raise EphemeralMentionError(
                 f"There is no record for that Player ID `{player_id}`; please make sure you have seeded on our servers previously and enter your Player ID (found in the top right of OPTIONS in game) to register.  Please open a ticket for additional help."
@@ -54,8 +52,6 @@ class BotCommands(commands.Cog):
     ) -> HLL_Player:
         try:
             return await HLL_Player.get(discord_id=discord_id)
-        except MultipleObjectsReturned:
-            raise EphemeralAdminError(f"Multiple discord_id's found for {discord_id=}")
         except DoesNotExist:
             message = f"Discord ID <@{discord_id}> is not registered. "
             register_cmd = command_mention(
