4faef74d7730bc0a0b3437570e4c39e8d758041e
Kevin Brasier <wonderfulweb@gmail.com>
2026-01-04 17:10:20 -0800

    Add hll-admin cmds register, unregister, check (#84)

--- src/seeding_reward_bot/commands/hll_admin.py
+++ src/seeding_reward_bot/commands/hll_admin.py
@@ -6,6 +6,7 @@ from discord.commands import SlashCommandGroup, option
 from tortoise.transactions import atomic
 
 from seeding_reward_bot.commands.util import BotCommands
+from seeding_reward_bot.db import HLL_Player
 from seeding_reward_bot.main import HLLDiscordBot
 
 
@@ -42,22 +43,40 @@ class HLLAdminCommands(BotCommands):
         )
         await ctx.respond("\n".join(message), ephemeral=True)
 
-    @hll_admin.command()
+    hll_admin_check = hll_admin.create_subgroup(
+        "check", "Admin-only commands to check VIP and seeding time."
+    )
+
+    @hll_admin_check.command(name="discord")
     @guild_only()
     @option("user", description="Discord user to get information about")
-    async def check_user(
+    async def check_discord(
         self, ctx: discord.ApplicationContext, user: discord.Member
     ) -> None:
-        """Admin-only command to check a user's VIP and seeding time."""
+        """Admin-only command to check a Discord user's VIP and seeding time."""
         await ctx.defer(ephemeral=True)
+        player = await self.get_player_by_discord_id(user.id, other=True)
+        await self._check(ctx, player)
 
-        vip, player = await self.get_vip_by_discord_id(user.id, other=True)
+    @hll_admin_check.command(name="player")
+    @guild_only()
+    @option("player_id", description="Player ID to get information about")
+    async def check_player(
+        self, ctx: discord.ApplicationContext, player_id: str
+    ) -> None:
+        """Admin-only command to check a player's VIP and seeding time."""
+        await ctx.defer(ephemeral=True)
+        player = await self.get_player_by_player_id(player_id, other=True)
+        await self._check(ctx, player)
+
+    async def _check(self, ctx: discord.ApplicationContext, player: HLL_Player) -> None:
+        vip = await self.get_vip_by_player_id(player.player_id, other=True)
 
         self.logger.debug(
             f'User {ctx.author.mention} is inspecting player data for "{player.discord_id}/{player.player_id}"'
         )
 
-        message = (f'Data for user "{user.mention}/{user.id}"',)
+        message = (f'Data for user "<@{player.discord_id}>/{player.discord_id}"',)
         if vip is None:
             message += ("VIP expiration: user has no active VIP via the RCON server.",)
         else:
@@ -65,9 +84,53 @@ class HLLAdminCommands(BotCommands):
             message += (f"VIP expiration: <t:{int(expiration.timestamp())}:R>",)
         message += (
             f"Database player name: `{player.player_name}`",
+            f"Database player ID: `{player.player_id}`",
             f"Last time seeded: <t:{int(player.last_seed_check.timestamp())}:R>",
             f"Current seeding balance (hours): `{player.seeding_time_balance // timedelta(hours=1):,}`",
             f"Total seeding time: `{player.total_seeding_time}`",
+        )
+        await ctx.respond("\n".join(message), ephemeral=True)
+
+    @hll_admin.command()
+    @option("user", description="Discord user to register")
+    @option(
+        "player_id",
+        description="Player ID to register",
+    )
+    async def register(
+        self, ctx: discord.ApplicationContext, user: discord.Member, player_id: str
+    ) -> None:
+        """Admin-only command to register a discord account to a Player ID"""
+        await ctx.defer(ephemeral=True)
+
+        await self.register_player(ctx, player_id, user, other=True)
+
+        await ctx.respond(
+            f"Registered `{player_id=}` to Discord account {user.mention}/{user.id}",
+            ephemeral=True,
+        )
+
+    @hll_admin.command()
+    @guild_only()
+    @option("user", description="Discord user to unregister")
+    @atomic()
+    async def unregister(
+        self, ctx: discord.ApplicationContext, user: discord.Member
+    ) -> None:
+        """Admin-only command to unregister a discord account from a Player ID."""
+        await ctx.defer(ephemeral=True)
+        player = await self.get_player_by_discord_id(user.id, other=True, update=True)
+        self.logger.debug(
+            f'User {ctx.author.mention} is unregistering "{player.discord_id}/{player.player_id}"'
+        )
+
+        player.discord_id = None
+        await player.save(update_fields=["discord_id"])
+
+        message = (
+            f"Successfully unregistered {user.mention}",
+            f"Player name was: `{player.player_name}`",
+            f"Player ID was: `{player.player_id}`",
         )
         await ctx.respond("\n".join(message), ephemeral=True)
 
